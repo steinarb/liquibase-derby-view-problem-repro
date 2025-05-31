309c3e11e9e0089a6e4b0b25f8105b5448563cb5
Steinar Bang <sb@dod.no>
2025-05-31 12:22:11 +0200

    Remove two unnecessary tests

--- src/test/java/no/priv/bang/repros/liquibaseviewderby/LiquibaseClassPathChangeLogRunnerTest.java
+++ src/test/java/no/priv/bang/repros/liquibaseviewderby/LiquibaseClassPathChangeLogRunnerTest.java
@@ -1,16 +1,12 @@
 package no.priv.bang.repros.liquibaseviewderby;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.db.api.Assertions.assertThat;
-import static org.mockito.Mockito.*;
-
 import javax.sql.DataSource;
 
 import org.apache.derby.jdbc.EmbeddedDataSource;
 import org.assertj.db.type.AssertDbConnectionFactory;
 import org.junit.jupiter.api.Test;
-import liquibase.exception.LiquibaseException;
 
 class LiquibaseClassPathChangeLogRunnerTest {
 
@@ -27,35 +23,6 @@ class LiquibaseClassPathChangeLogRunnerTest {
 
         var accounts2 = assertjConnection.request("select * from measures_view").build();
         assertThat(accounts2).hasNumberOfColumns(22);
-    }
-
-    @Test
-    void testCreateSchemaAndFail() throws Exception {
-        var datasource = createDataSource("sampleapp");
-        var connection = spy(datasource.getConnection());
-        // A Derby JDBC connection wrapped in a Mockito spy() fails om Connection.setAutoClosable()
-
-        var sampleappLiquibase = new LiquibaseClassPathChangeLogRunner();
-
-        var ex = assertThrows(
-            LiquibaseException.class,
-            () -> sampleappLiquibase.createInitialSchema(connection));
-        assertThat(ex.getMessage()).startsWith("java.sql.SQLException: Cannot set Autocommit On when in a nested connection");
-    }
-
-    @Test
-    void testCreateSchemaAndFailOnConnectionClose() throws Exception {
-        var datasource = createDataSource("sampleapp2");
-        var connection = spy(datasource.getConnection());
-        doNothing().when(connection).setAutoCommit(anyBoolean());
-        doThrow(Exception.class).when(connection).close();
-
-        var sampleappLiquibase = new LiquibaseClassPathChangeLogRunner();
-
-        var ex = assertThrows(
-            LiquibaseException.class,
-            () -> sampleappLiquibase.createInitialSchema(connection));
-        assertThat(ex.getMessage()).startsWith("java.lang.Exception");
     }
 
     private DataSource createDataSource(String dbname) throws Exception {
